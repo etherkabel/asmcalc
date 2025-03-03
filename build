e60e8973cb6fc44e9dbe17720a4a2826609a1e09
IsmaRUS <gmgmir34@mail.ru>
2025-03-03 18:14:35 +0300

    сделал скрипт для сборки:    build 	изменил код (всё равно не работает):      main.asm

---
+++ build
@@ -0,0 +1,2 @@
+nasm -f elf64 main.asm -o main.o
+ld main.o -o main
--- main.asm
+++ main.asm
@@ -1,27 +1,51 @@
 global _start
 
 section .bss
-a resb 16
-b resb 16
+a resw 1
+b resw 1
 
 section .text
 _start:
 	mov rax, 0
 	mov rdi, 0
 	mov rsi, a
-	mov rdx, 16
-	syscall 
+	mov rdx, 2
+	syscall
+
+	mov [a + 1], 0
 
 	mov rax, 0
 	mov rdi, 0
 	mov rsi, b
-	mov rdx, 16
-	syscall 
+	mov rdx, 2
+	syscall
+
+	mov [b + 1], 0
+
+	push [a]
+	call str_to_int
+	pop [a]
+
+	push [b]
+	call str_to_int
+	pop [b]
 
 	mov ax, [a]
 	mov bx, [b]
 	add ax, bx
+
+	xor rdi, rdi
 	mov di, ax
+	jmp exit
+str_to_int:
+    mov rsi, rsp
+    sub [rsi + 0], '0'
+    sub [rsi + 1], '0'
+    sub [rsi + 2], '0'
+    pop eax
+    xor eax, eax
+    push rsi
+    ret
 exit:
 	mov rax, 60
 	syscall
