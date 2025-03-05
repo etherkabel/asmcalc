b849583b6f50a1d6a72f24fda653cb5b7154f42e
IsmaRUS <gmgmir34@mail.ru>
2025-03-05 22:37:18 +0300

    изменен: я чуть разобрался, завтра доделаю

--- main.asm
+++ main.asm
@@ -1,51 +1,31 @@
 global _start
 
 section .bss
-a resw 1
-b resw 1
+a resb 64
+b resb 64
+result resb 64
+
+; цифры в ascii таблице - от 48 (0) до 57 (9)
 
 section .text
 _start:
-	mov rax, 0
-	mov rdi, 0
-	mov rsi, a
-	mov rdx, 2
-	syscall
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, a
+    mov rdx, 64
+    syscall
 
-	mov [a + 1], 0
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, b
+    mov rdx, 64
+    syscall
 
-	mov rax, 0
-	mov rdi, 0
-	mov rsi, b
-	mov rdx, 2
-	syscall
+    xor rcx, rcx
+a_to_int:
+    cmp byte [a + rcx], 0
+    je b_to_int
+    movzx rax, byte [a + rcx]
+    sub rax, '0'
 
-	mov [b + 1], 0
-
-	push [a]
-	call str_to_int
-	pop [a]
-
-	push [b]
-	call str_to_int
-	pop [b]
-
-	mov ax, [a]
-	mov bx, [b]
-	add ax, bx
-
-	xor rdi, rdi
-	mov di, ax
-	jmp exit
-str_to_int:
-    mov rsi, rsp
-    sub [rsi + 0], '0'
-    sub [rsi + 1], '0'
-    sub [rsi + 2], '0'
-    pop eax
-    xor eax, eax
-    push rsi
-    ret
-exit:
-	mov rax, 60
-	syscall
+    ; ФОРМУЛА - ((ASCII КОД) - 48) * 10^N + ...
