ed98ca09dd16f6152f3fca7f510ae007b9cd6bcb
IsmaRUS <gmgmir34@mail.ru>
2025-03-09 13:19:11 +0300

    Исправлены некоторые недочёты библиотеки

--- trn.c
+++ trn.c
@@ -1,17 +1,34 @@
 #include <stdint.h>
+#include <string.h>
 
 void trn_int(char* source) {
-    *(source + (sizeof(source))) = '\0';
-    for (int i=0;*(source + i) != '\0';++i) {
-        *(source + i) -= 48;
+    size_t lenght = strlen(source);
+    source[lenght] = '\0';
+    for (size_t i=0;i < lenght;++i) {
+        if (source[i] >= '0' && source[i] <= '9')
+            source[i] -= '0';
     }
 }
 
 void trn_str(int64_t* source, char* result) {
-    int source_rem = 0;
-    for (int i=0;*source != 0;++i) {
-        source_rem = *source % 10;
-        *(result + i) = source_rem += 48;
+    if (source == NULL || *source == 0) {
+        strcpy(result, "0");
+        return;
+    }
+
+    int index = 0;
+    while (*source > 0) {
+        int digit = *source % 10;
+        result[index++] = digit + '0';
         *source /= 10;
+    }
+
+    result[index] = '\0';
+
+
+    for (int i = 0; i < index / 2; ++i) {
+        char temp = result[i];
+        result[i] = result[index - i - 1];
+        result[index - i - 1] = temp;
     }
 }
